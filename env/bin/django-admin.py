9af59ce96f937cc66ffd9e33abc08e18cc359340
Tristan Montemitro <tmontemitro@gmail.com>
2023-08-18 19:07:30 +0200

    Exclude .env from version control

--- env/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/Tristanmontemitro/Desktop/dev/openclassrooms/P13_montemitro_tristan/OPC_DA_P13_ORANGE_COUNTY_LETTINGS_DEVOPS/env/bin/python3.11
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()

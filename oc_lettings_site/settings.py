285ea77db7cd905b285dfd1207b5c7df9cc0df32
Tristan Montemitro <tmontemitro@gmail.com>
2023-08-20 14:55:22 +0200

    image docker et static terminés

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -4,6 +4,10 @@ from pathlib import Path
 
 import sentry_sdk
 from sentry_sdk.integrations.django import DjangoIntegration
+from dotenv import load_dotenv
+
+
+load_dotenv()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -13,10 +17,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = str(os.environ.get('DEBUG')) == '1'
+# DEBUG = False
+DEBUG = os.getenv("DEBUG", False) == True
 
 ALLOWED_HOSTS = ["*"]
 

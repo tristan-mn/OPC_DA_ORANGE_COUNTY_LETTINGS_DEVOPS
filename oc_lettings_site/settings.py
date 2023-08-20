281c866dd9ebce8bd36fb6c25965633c37025991
Tristan Montemitro <tmontemitro@gmail.com>
2023-08-20 15:21:01 +0200

    corretion settings pour flake8

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -20,8 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = False
-DEBUG = os.getenv("DEBUG", False) == True
+DEBUG = os.getenv("DEBUG", '0') == '1'
 
 ALLOWED_HOSTS = ["*"]
 
